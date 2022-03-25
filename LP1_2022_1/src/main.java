29b36aff7fb6c93f8d2a4330cf442d662e1b6cc0
Crescencio Lima <crescencio@gmail.com>
2022-03-25 14:46:38 -0300

    Upload dos arquivos da discplina semestre 2022.1

---
+++ LP1_2022_1/src/main.java
@@ -0,0 +1,19 @@
+import java.util.Calendar;
+
+public class main {
+
+	public static void main(String[] args) {
+		int x, y, m, n;
+		m = 5;
+		n = 5;
+		x = ++m;
+		System.out.printf("M = %d X = %d", m, x);
+		y = n++;
+		System.out.printf("\nN = %d Y = %d", n, y);
+		
+		Calendar calendario = Calendar.getInstance();
+		
+		
+	}
+
+}
